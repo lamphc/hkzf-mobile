5d68eb30c4bdb0223fc86cc86f8b6642c03d92d6
lamphc <lamphc@live.com>
2020-01-06 16:23:03 +0800

    feat: 去除ant全局样式，按需加载

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Button } from 'antd-mobile';
 
-
-
-
 function App() {
   return (
     <div className="App">
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'antd-mobile/dist/antd-mobile.css';
+// import 'antd-mobile/dist/antd-mobile.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
