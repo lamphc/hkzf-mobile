7738ba211144646feb5751bc6693f748c65b2122
lamphc <lamphc@live.com>
2020-01-06 18:39:26 +0800

    feat: 修改按需配置

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,7 @@
 const { override, fixBabelImports } = require('customize-cra');
-module.exports = function override(config, env) {   // do stuff with the webpack config...
-  return config;
-};
 module.exports = override(
   fixBabelImports('import', {
     libraryName: 'antd-mobile',
-    style: 'css',
-  }),
+    style: 'css'
+  })
 );
