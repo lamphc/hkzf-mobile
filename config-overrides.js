98d01630529679b26bfc766e59adab483ddb7391
lamphc <lamphc@live.com>
2020-01-06 16:05:55 +0800

    feat: 按需加载配置

---
+++ config-overrides.js
@@ -0,0 +1,10 @@
+const { override, fixBabelImports } = require('customize-cra');
+module.exports = function override(config, env) {   // do stuff with the webpack config...
+  return config;
+};
+module.exports = override(
+  fixBabelImports('import', {
+    libraryName: 'antd-mobile',
+    style: 'css',
+  }),
+);
