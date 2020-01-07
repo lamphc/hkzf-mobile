aefe2b22eedb63392730ae023d1cd2ccebf07cd4
lamphc <lamphc@live.com>
2020-01-07 13:07:05 +0800

    feat: 配置全局菜单

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 
 import { Route, Link } from 'react-router-dom';
 
+import { TabBar } from 'antd-mobile';
+import tabBars from '../../utils/tabbar_config';
+
+import '../../assets/fonts/iconfont.css'
+import './index.css'
+
 import Index from '../Index';
 import House from '../House';
 import Profile from '../Profile';
@@ -11,16 +17,67 @@ import Profile from '../Profile';
  * 2. 找房
  * 3. 我的（个人中心）
  */
+
+
+
+
 class Home extends Component {
+
+  state = {
+    selectedTab: this.props.location.pathname,
+  };
+
+
+  renderTabBarItems = () => {
+    return tabBars.map((item, index) => <TabBar.Item
+      title={item.title}
+      key={index}
+      icon={
+        <i className={'iconfont ' + item.icon} />
+      }
+      selectedIcon={<i className={'iconfont ' + item.icon} />
+      }
+      selected={this.state.selectedTab === item.path}
+      onPress={() => {
+        this.setState({
+          selectedTab: item.path,
+        }, () => {
+          this.props.history.push(item.path)
+        });
+      }}
+
+    />)
+  }
+
+
+
   render() {
+    // console.log(this.props);
     return (
       <div>
-        <Link to="/home/index">首页</Link>
+        {/* <Link to="/home/index">首页</Link>
         <Link to="/home/house">找房</Link>
-        <Link to="/home/profile">我的</Link>
+       <Link to="/home/profile">我的</Link> */}
         <Route path="/home/index" component={Index} />
         <Route path="/home/house" component={House} />
         <Route path="/home/profile" component={Profile} />
+        <div className="barBox">
+          <TabBar
+            unselectedTintColor="#949494"
+            tintColor="#33A3F4"
+            barTintColor="white"
+            tabBarPosition="bottom"
+            noRenderContent={true}
+          >
+
+            {
+              this.renderTabBarItems()
+            }
+
+
+
+          </TabBar>
+        </div >
       </div>
     );
   }
---
+++ src/utils/tabbar_config.js
@@ -0,0 +1,24 @@
+/**
+ * 底部菜单数据
+ * 
+ */
+
+const tabBars = [
+  {
+    title: '首页',
+    path: '/home/index',
+    icon: 'icon-ind'
+  },
+  {
+    title: '找房',
+    path: '/home/house',
+    icon: 'icon-findHouse'
+  },
+  {
+    title: '我的',
+    path: '/home/profile',
+    icon: 'icon-my'
+  },
+]
+
+export default tabBars
