e96ab598fda4be8097b7db89ae6a033a5c5c6d70
lamphc <lamphc@live.com>
2020-02-09 21:28:56 +0800

    feat: 绘制覆盖物

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { NavBar, Icon } from 'antd-mobile';
 
 import styles from './index.module.css'
+import { getCurrCity } from '../../utils';
 
 class Map extends Component {
 
@@ -9,14 +10,48 @@ class Map extends Component {
     this.initMap()
   }
   // 初始化地图
-  initMap = () => {
+  initMap = async () => {
     const { BMap } = window;
-    var map = new BMap.Map("container");
-    // 创建地图实例  
-    var point = new BMap.Point(116.404, 39.915);
-    // 创建点坐标  
-    map.centerAndZoom(point, 15);
-    // 初始化地图，设置中心点坐标和地图级别  
+    // 创建地图实例 
+    let map = new BMap.Map("container");
+    // 获取定位城市
+    const { value, lable } = await getCurrCity();
+    // 创建地址解析器实例     
+    let myGeo = new BMap.Geocoder();
+    // 将地址解析结果显示在地图上，并调整地图视野    
+    myGeo.getPoint(lable, function (point) {
+      if (point) {
+        // 初始化地图，设置中心点坐标和地图级别  
+        map.centerAndZoom(point, 11);
+        // 添加控件
+        map.addControl(new BMap.NavigationControl());
+        map.addControl(new BMap.ScaleControl());
+        map.addOverlay(new BMap.Marker(point));
+        const opts = {
+          position: point,    // 指定文本标注所在的地理位置
+          offset: new BMap.Size(0, 0)    //设置文本偏移量
+        }
+        const label = new BMap.Label(null, opts);  // 创建文本标注对象
+        label.setContent(
+          `
+          <div class="${styles.bubble}">
+          <p class="${styles.bubbleName}">浦东新区</p>
+          <p>388套</p>
+          </div>
+          `
+        )
+        // 去除默认样式
+        label.setStyle({
+          border: 'none'
+        });
+        // 添加点击事件
+        label.addEventListener('click', () => {
+          console.log('覆盖物被点击了', point)
+        })
+        map.addOverlay(label);
+      }
+    },
+      lable);
   }
   render() {
     return (
