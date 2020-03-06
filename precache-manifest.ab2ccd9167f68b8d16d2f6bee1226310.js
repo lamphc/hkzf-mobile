5444218bf95cbac112f152cb8b02190f9e7ac5d3
lamphc <lamphc@live.com>
2020-03-06 19:09:04 +0800

    Updates

--- precache-manifest.ab2ccd9167f68b8d16d2f6bee1226310.js
+++ precache-manifest.ab2ccd9167f68b8d16d2f6bee1226310.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1972e39e32707892582e84a1e720a839",
+    "revision": "9a9d998890319566e5e0945638ce9f85",
     "url": "/hkzf-mobile/index.html"
   },
   {
@@ -60,7 +60,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/hkzf-mobile/static/css/9.d4867ff4.chunk.css"
   },
   {
-    "revision": "8d54af977aea5ad6381f",
+    "revision": "ced6f0304721da5fce90",
     "url": "/hkzf-mobile/static/css/main.02ea8f1c.chunk.css"
   },
   {
@@ -128,8 +128,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/hkzf-mobile/static/js/9.1d51720e.chunk.js"
   },
   {
-    "revision": "8d54af977aea5ad6381f",
-    "url": "/hkzf-mobile/static/js/main.84167af8.chunk.js"
+    "revision": "ced6f0304721da5fce90",
+    "url": "/hkzf-mobile/static/js/main.150a85b7.chunk.js"
   },
   {
     "revision": "1e4f20fa3b8ef33c9bf5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hkzf-mobile/precache-manifest.e97dd0aae3da9c47cbe0badaf9e661a8.js"
+  "/hkzf-mobile/precache-manifest.ab2ccd9167f68b8d16d2f6bee1226310.js"
 );
 
 self.addEventListener('message', (event) => {
